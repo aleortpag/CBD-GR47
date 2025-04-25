7c780bf6f4fef4926416a0836823b7dab77b2087
aleortpag <a.ortiz.pagador@gmail.com>
2025-04-25 16:34:45 +0200

    Base de datos poblada

--- database.js
+++ database.js
@@ -1,24 +1,83 @@
 const { MongoClient } = require('mongodb');
 
 const MONGO_URI = 'mongodb://localhost:27017';
-const DB_NAME = '';
-const COLLECTION_NAME = '';
+const DB_NAME = 'cbd';
+const COLLECTION_NAME = 'juegos';
 
+let client = new MongoClient(MONGO_URI);
+let collection;
+let conectado = false;
 
-async function poblar_database() {
-    const client = new MongoClient(MONGO_URI);
-
-    try{
-        await client.connect();
-        ipcRenderer.send('mostrar-mensaje', "Conectado a la db");
-
-        
-    }catch{
-        ipcRenderer.send('mostrar-error', "Error al conectar a la db");
-    }finally{
-
+async function abrirConexion() {
+    try {
+        if (conectado) {
+            ipcRenderer.send('mostrar-mensaje', "Ya estas conectado a MongoDB");
+        } else {
+            await client.connect();
+            const db = client.db(DB_NAME);
+            collection = db.collection(COLLECTION_NAME);
+            ipcRenderer.send('mostrar-resultado', "Conectado a Mongodb");
+            conectado = true;
+        }
+    } catch (error) {
+        ipcRenderer.send('mostrar-error', error);
     }
-
 }
 
-module.exports = { poblar_database };
+async function cerrarConexion() {
+    try {
+        if (!conectado) {
+            ipcRenderer.send('mostrar-mensaje', "La conexion ya estaba cerrada");
+        } else {
+            await client.close();
+            ipcRenderer.send('mostrar-resultado', "Conexion a Mongodb cerrada");
+            conectado = false;
+        }
+    } catch (error) {
+        ipcRenderer.send('mostrar-error', error);
+    }
+}
+
+async function guardarJuego(juego) {
+    const existente = await collection.findOne({ nombre: juego.nombre });
+    try {
+        if (!conectado) {
+            ipcRenderer.send('mostrar-error', "No hay conexion con la db");
+        } else if (existente) {
+            ipcRenderer.send('mostrar-error', "El juego " + juego.nombre + " ya existe en db");
+        } else {
+            await collection.insertOne(juego);
+            ipcRenderer.send('mostrar-resultado', "El juego " + juego.nombre + " se ha guardado en db");
+        }
+    } catch (error) {
+        ipcRenderer.send('mostrar-error', error);
+    }
+}
+
+async function limpiarDB() {
+    try {
+        if (!conectado) {
+            ipcRenderer.send('mostrar-error', "No hay conexion con la db");
+        } else {
+            await collection.deleteMany({});
+            ipcRenderer.send('mostrar-resultado', "Se ha vaciado la db");
+        }
+    } catch {
+        ipcRenderer.send('mostrar-error', error);
+    }
+}
+
+async function buscarJuegosPorNombre(nombre) {
+    try {
+        if (!conectado) {
+            ipcRenderer.send('mostrar-error', "No hay conexion con la db");
+        } else {
+            const juegos = await collection.find({}).toArray();
+            return juegos;
+        }
+    } catch (error) {
+        ipcRenderer.send('mostrar-error', error);
+    }
+}
+
+module.exports = { abrirConexion, cerrarConexion, guardarJuego, limpiarDB, buscarJuegosPorNombre };
--- renderer.js
+++ renderer.js
@@ -1,12 +1,23 @@
 const { ipcRenderer } = require('electron');
-const {poblarDatabase} = require('./scrapping.js')
+const { abrirConexion, cerrarConexion, limpiarDB } = require('./database.js');
+const { scrapping } = require('./scrapping.js');
 
-ipcRenderer.send('mostrar-mensaje', "ENTRA EN JS");
-poblarDatabase();
 document.addEventListener("DOMContentLoaded", () => {
     const boton = document.getElementById("btn-poblar-db");
-    const div = document.getElementById("mensaje-pruebas");
+    const div = document.getElementById("div-resultados");
 
-    
-    
+    boton.addEventListener('click', poblarDB);
 });
+
+async function poblarDB() {
+  ipcRenderer.send('mostrar-mensaje', "Boton pulsado");
+  await abrirConexion();
+  await limpiarDB();
+  await scrapping();
+  await cerrarConexion();
+  ipcRenderer.send('mostrar-resultado', "Se ha completado la carga de la db");
+}
+
+async function mostrarJuegos(){
+  
+}
